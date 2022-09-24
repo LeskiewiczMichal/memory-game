df0d4e3df23b9a05a66fdbdf9cea0112028a0b6c
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-09-24 12:56:52 +0200

    randomize cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Header from './components/Header';
 import Card from './components/Card';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 // import images of flags
 import america from './images/america.png';
 import brazil from './images/brazil.png';
@@ -16,59 +18,64 @@ import turkish from './images/turkish.png';
 import ukraine from './images/ukraine.png';
 
 function App() {
-  const countryFlags = [
-    america,
-    brazil,
-    china,
-    england,
-    france,
-    italy,
-    norway,
-    portugal,
-    southKorea,
-    swedish,
-    turkish,
-    ukraine,
-  ];
-  const countryNames = [
-    'America',
-    'Brazil',
-    'China',
-    'England',
-    'France',
-    'Italy',
-    'Norway',
-    'Portugal',
-    'South Korea',
-    'Swedish',
-    'Turkish',
-    'Ukraine',
+  const countries = [
+    { image: america, name: 'America' },
+    { image: brazil, name: 'Brazil' },
+    { image: china, name: 'China' },
+    { image: england, name: 'England' },
+    { image: france, name: 'France' },
+    { image: italy, name: 'Italy' },
+    { image: norway, name: 'Norway' },
+    { image: portugal, name: 'Portugal' },
+    { image: southKorea, name: 'South Korea' },
+    { image: swedish, name: 'Swedish' },
+    { image: turkish, name: 'Turkish' },
+    { image: ukraine, name: 'Ukraine' },
   ];
 
   // map through array of images and create card components from data
   // randomize order of array
+  const cards = countries.map((country) => {
+    return (
+      <Card
+        image={country.image}
+        countryName={country.name}
+        id={nanoid()}
+        handleClick={handleClick}
+      />
+    );
+  });
+
+  function randomizeArr(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
+  randomizeArr(cards)
 
   // clickedCards [id of clicked cards]
+  const [clickedCards, setClickedCards] = useState([]);
   // if clickedcards contains clicked card id lost else point +1
+  function handleClick(id) {
+    alert(id);
+  }
 
   return (
     <div className="app--container">
       <Header />
-      <div className="app--cards-container">
-        <Card image={china} countryName='South Korea'/>
-        <Card image={china} countryName='italy'/>
-        <Card image={china} countryName='italy'/>
-        <Card image={china} countryName='italy'/>
-        <Card image={china} countryName='italy'/>
-        <Card image={china} countryName='italy'/>
-        <Card image={china} countryName='italy'/>
-        <Card image={china} countryName='italy'/>
-        <Card image={china} countryName='italy'/>
-        <Card image={china} countryName='italy'/>
-        <Card image={china} countryName='italy'/>
-        <Card image={china} countryName='italy'/>
-
-      </div>
+      <div className="app--cards-container">{cards}</div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import './style/card.css';
 
 export default function Card(props) {
     return (
-        <div className='card--container'>
+        <div className='card--container' onClick={() => props.handleClick(props.id)}>
             <img className='card--image' src={props.image} alt="ok" />
             <h5 className='card--country-name'>{props.countryName}</h5>
         </div>
